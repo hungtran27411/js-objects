2cef86072d66ce410d69ec5a3aaf5bd6172aac31
Hung <hungtran27411@icloud.com>
2024-01-25 22:45:20 -0800

    js-objects-homework

--- js/app.js
+++ js/app.js
@@ -1,49 +1,152 @@
-console.log('js loaded')
-const game = {
-    title: 'Guess the number!',
-}
-console.log(game)
 
-//get values
-game.title // this would get the value "Guess the number"
-// of we want to log to the console
-console.log(game.title)
-console.log(game['title'])
+const album1 = {
+    title: 'Talking Heads',
+    albumDetails: {
+      released: new Date('September 16, 1977'),
+      label: 'Sire',
+      formats: ['LP']
+    }
+  };
+ 
+  // Exercise 1:  Update the title property of album1 from 'Talking Heads' to 'Talking Heads - 77', then assign that property to a variable named title
+  
+  album1.title = 'Talking Heads - 77';
+  let title = album1.title;
 
-// set values
-game.biggestNum = 50;
+  //console.log(title)
 
-game.lowestNum = 10;
-game['guessedNumber'] = 6;
-//set values
+  // Exercise 2: Assign the string 'Sire' from album1 to a variable named label
+  let label = album1.albumDetails.label;
+  
+  console.log(label)
+  
+  const album2 = {
+    title: 'More Songs About Buildings and Food',
+    albumDetails: {
+      released: new Date('July 14, 1978'),
+      label: 'Sire',
+      formats: ['LP', '8-track'],
+    }
+  };
+ 
+  
 
-console.log(game)
+
+  const album3 = {
+    
+    title: 'Fear of Music',
+    albumDetails: {
+    released: 'August 3, 1979',
+      label: 'Sire',
+      formats: ['Cassette']
+    }
+  };
+  
+  // Exercise 3: Accessing the string 'LP' from album2's formats array, add it to the end of album3's formats array.
+  album3.albumDetails.formats.push('LP');
+
+  console.log(album3.albumDetails.formats.push('LP'))
+  
+  
+  // Exercise 4:  Update the released property of album3 from a string into a Date object using that string
+ 
+ album3.albumDetails.released  = new Date
+ console.log('Exercise4',album3.albumDetails.released  = new Date) 
+
+  
+  
+  const album4 = {
+    title: 'Remain in Light',
+    albumDetails: {
+      released: new Date('October 8, 1980'),
+      formats: ['Cassette', 'LP']
+    }
+  };
+  
+  // Exercise 5:  Add a property named label with the value 'Sire' to album4's albumDetails property
+ album4.albumDetails.label = 'Sire'
+ console.log('Exercis5',album4.albumDetails.label)
+  
+  
+  
+  const album5 = {
+    title: 'Little Creatures',
+    albumDetails: {
+      released: new Date('June 10, 1985'),
+      labels: ['Sire', 'emi'],
+      formats: ['CD', 'cassette', 'LP']
+    }
+  };
+  
+  // Exercise 6:  Update the value 'emi' within album5's labels array to 'EMI'
+  album5.albumDetails.labels[1] = album5.albumDetails.labels[1].toUpperCase()
+  console.log('Exercise6',album5.albumDetails.labels[1].toUpperCase())
+  
+  
+  
+  const album6 = {
+    title: 'True Stories',
+    albumDetails: {
+      released: new Date('October 7, 1986'),
+      labels: ['Sire, EMI'],
+      formats: ['CD', 'cassette', 'LP']
+    }
+  };
+  
+  // Exercise 7:  Assign album6's formats array to a variable named formats
+  //console.log(Object.values(album6.albumDetails.formats));
+  const formats = album6.albumDetails.formats;
 
 
+  const album7 = {
+    title: 'Naked',
+    albumDetails: {
+      released: new Date('March 15, 1988'),
+      labels: ['Sire', 'EMI'],
+      formats: ['CD', 'cassette', 'LP']
+    }
+  };
+  
+  const talkingHeadsAlbums = [
+    album1,
+    album2,
+    album3,
+    album4,
+    album5,
+    album6,
+    album7
+  ];
+  
+  // Exercise 8:  Using the talkingHeadsAlbums array, assign album5's labels property to a variable named labels
+  
+  const labels = talkingHeadsAlbums[4].albumDetails.labels;
+  
+  
+  // Exercise 9:  Using the talkingHeadsAlbums array, assign album7's released property to album6's released property
+  
+  talkingHeadsAlbums[5].albumDetails.released = talkingHeadsAlbums[6].albumDetails.released;
 
-const instagrampost = {
-    username: 'hung',
-    isVerified: true,
-    description:'comments',
-    pictures: ['pic1', 'pic2','pic3'],
-    comments: [
-        {username: 'username1', comments: 'Why you use a rope'},
-        {username: 'username2', comments: '13 a warm up'},
-        {username: 'username3', comments: 'Alex delete last pic'},
-    print.isVerified = !true
-    ]
-}
-instagrampost.comments.forEach(comments => {console.log(comments.username)})
+  
+  
+  // Exercise 10:  Using the pre-defined variable named albumIdx below, assign the albumDetails object of the album located within the talkingHeadsAlbums array at the index represented by the value of albumIdx to a variable named albumDetails
+  
+  let albumIdx = 4;
+  
+  let albumDetails = talkingHeadsAlbums;
+  
+  
+  /********** Don't look below here **********/
+  console.log('=========================== Excercise console.logs =============')
+  console.log('Exercise 1:', title);
+  console.log('Exercise 2:', label);
+  console.log('Exercise 3:', album3.albumDetails.formats[1]);
+  console.log('Exercise 4:', album3.albumDetails.released.toLocaleDateString());
+  console.log('Exercise 5:', album4.albumDetails.label);
+  console.log('Exercise 6:', album5.albumDetails.labels[1]);
+  console.log('Exercise 7:', formats);
+  console.log('Exercise 8:', labels);
+  console.log('Exercise 9:', talkingHeadsAlbums[5].albumDetails.released.toLocaleDateString());
+  console.log('Exercise 10:', albumDetails);
+  console.log('===================================================================')
 
-// print pic3
-console.log(instagrampost.pictures[2]);
-// set a new value for the caption
-instagrampost.caption = 'my new caption';
-// print out 13 a warm up
-console.log(instagrampost.comments[1].comment)
-//set isVerified to its opposite value
-instagrampost.isVerified = !instagrampost.isVerified
-//Print out each username in the comments using the forEach metho
-instagrampost.comments.forEach(function(comment){
-	console.log(comment.username)
-})
+  
